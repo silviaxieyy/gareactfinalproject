9a8b5549942db1f623cb5f1f66721a24713f782e
yuanyuan <silviaxieyy@gmail.com>
2024-04-10 16:30:19 +1000

    delete leaflet related

--- pages/clubs/index.js
+++ pages/clubs/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import dynamic from 'next/dynamic';
 import DashboardLayout from '../../components/Layout';
-import MapMarker from '../../components/MapMarker';
 
-const DynamicMap = dynamic(() => import("../../components/MapMarker"), {
-  ssr: false,
-})
 
 const Clubs = () => {
   return (
@@ -15,7 +10,6 @@ const Clubs = () => {
           <div className='mt-2 mb-0'>
             <h1 className='font-russo-one text-4xl font-bold text-gray-400 mx-auto'>Club choices</h1>
           </div>
-          <DynamicMap />
         </div>
       </DashboardLayout>
     </div>
